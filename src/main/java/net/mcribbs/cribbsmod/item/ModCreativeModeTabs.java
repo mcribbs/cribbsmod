10054659dfc7ba0e547c4b8c6ac2203696672fda
Matt Cribbs <mcribbs@gmail.com>
2024-07-15 17:52:37 -0400

    Advanced block
    https://www.youtube.com/watch?v=HCbmrPkDV7A

--- src/main/java/net/mcribbs/cribbsmod/item/ModCreativeModeTabs.java
+++ src/main/java/net/mcribbs/cribbsmod/item/ModCreativeModeTabs.java
@@ -34,6 +34,8 @@ public class ModCreativeModeTabs {
                         pOutput.accept(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
                         pOutput.accept(ModBlocks.NETHER_SAPPHIRE_ORE.get());
                         pOutput.accept(ModBlocks.END_STONE_SAPPHIRE_ORE.get());
+
+                        pOutput.accept(ModBlocks.SOUND_BLOCK.get());
                     })
                     .build());
 
